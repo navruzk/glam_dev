5dbc83d4378f2f1feea74442b6e9717c79b5d56e
navruzbek1992 <40276486+navruzbek1992@users.noreply.github.com>
2018-11-11 23:32:47 +0900

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,48 +1,64 @@
-glam_myhfunc1 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc1`, numeric, is_code)
+rcpp_glam_myhfunc1 <- function(x,y) {
+    val = glam_myhfunc1(x,y)
+    val
 }
-glam_myhfunc2 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+rcpp_glam_myhfunc2 <- function(x,y) {
+    val = glam_myhfunc2(x,y)
+    val
 }
-glam_myhfunc3 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_myhfunc3 <- function(x,y) {
+    val = glam_myhfunc3(x,y)
+    val
 }
-glam_myslopefunc <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myslopefunc`, numeric, is_code)
+rcpp_glam_myhfunc4 <- function(x,y) {
+    val = glam_myhfunc4(x,y)
+    val
 }
-glam_mydiff1 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_myhfunc5 <- function(x,y) {
+    val = glam_myhfunc5(x,y)
+    val
 }
-glam_mydiff2 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_myhfunc6 <- function(x,y) {
+    val = glam_myhfunc6(x,y)
+    val
 }
-glam_mydiff3 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_myslopefunc <- function(x) {
+    val = glam_myslopefunc(x)
+    val
 }
-glam_myhfunc4 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc1`, numeric, is_code)
+rcpp_glam_mydiff1 <- function(x,y) {
+    val = glam_mydiff1(x,y)
+    val
 }
-glam_myhfunc5 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+rcpp_glam_mydiff2 <- function(x,y) {
+    val = glam_mydiff2(x,y)
+    val
 }
-glam_myhfunc6 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_mydiff3 <- function(x,y) {
+    val = glam_mydiff3(x,y)
+    val
 }
-glam_scorefunctheta1 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+rcpp_glam_scorefunctheta1 <- function(x,y) {
+    val = glam_scorefunctheta1(x,y)
+    val
 }
-glam_scorefuncmu1 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_scorefuncmu1 <- function(x,y) {
+    val = glam_scorefuncmu1(x,y)
+    val
 }
-glam_scorefunctheta2 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+rcpp_glam_scorefunctheta2 <- function(x,y) {
+    val = glam_scorefunctheta2(x,y)
+    val
 }
-glam_scorefuncmu2 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_scorefuncmu2 <- function(x,y) {
+    val = glam_scorefuncmu2(x,y)
+    val
 }
-glam_scorefunctheta3 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+rcpp_glam_scorefunctheta3 <- function(x,y) {
+    val = glam_scorefunctheta3(x,y)
+    val
 }
-glam_scorefuncmu3 <- function(numeric, is_code = FALSE) {
-    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+rcpp_glam_scorefuncmu3 <- function(x,y) {
+    val = glam_scorefuncmu3(x,y)
+    val
 }
