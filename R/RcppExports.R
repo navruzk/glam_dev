3fe468a8806c56fcc8ec27feb2b1b1829cd7f22b
navruzbek1992 <40276486+navruzbek1992@users.noreply.github.com>
2018-11-11 23:13:17 +0900

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,3 +1,48 @@
 glam_myhfunc1 <- function(numeric, is_code = FALSE) {
     .Call(`_glam_glam_myhfunc1`, numeric, is_code)
 }
+glam_myhfunc2 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+}
+glam_myhfunc3 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_myslopefunc <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myslopefunc`, numeric, is_code)
+}
+glam_mydiff1 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_mydiff2 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_mydiff3 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_myhfunc4 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc1`, numeric, is_code)
+}
+glam_myhfunc5 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+}
+glam_myhfunc6 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_scorefunctheta1 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+}
+glam_scorefuncmu1 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_scorefunctheta2 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+}
+glam_scorefuncmu2 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
+glam_scorefunctheta3 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc2`, numeric, is_code)
+}
+glam_scorefuncmu3 <- function(numeric, is_code = FALSE) {
+    .Call(`_glam_glam_myhfunc3`, numeric, is_code)
+}
