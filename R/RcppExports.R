00978365e27467db013afb3ea7762c5eed0f858c
navruzbek <40276486+navruzbek1992@users.noreply.github.com>
2018-11-12 13:13:37 +0900

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,68 +1,70 @@
-rcpp_glam_add <- function(x,y) {
-    val = glam:::glam_add(x,y)
-    val
+# rcpp_glam_add <- function(x,y) {
+#     val = glam:::glam_add(x,y)
+#     val}
+glam_add <- function(orig) {
+    .Call(`_glam_glam_add`, orig)
 }
-rcpp_glam_myhfunc1 <- function(x,y) {
-    val = glam_myhfunc1(x,y)
-    val
-}
-rcpp_glam_myhfunc2 <- function(x,y) {
-    val = glam_myhfunc2(x,y)
-    val
-}
-rcpp_glam_myhfunc3 <- function(x,y) {
-    val = glam_myhfunc3(x,y)
-    val
-}
-rcpp_glam_myhfunc4 <- function(x,y) {
-    val = glam_myhfunc4(x,y)
-    val
-}
-rcpp_glam_myhfunc5 <- function(x,y) {
-    val = glam_myhfunc5(x,y)
-    val
-}
-rcpp_glam_myhfunc6 <- function(x,y) {
-    val = glam_myhfunc6(x,y)
-    val
-}
-rcpp_glam_myslopefunc <- function(x) {
-    val = glam_myslopefunc(x)
-    val
-}
-rcpp_glam_mydiff1 <- function(x,y) {
-    val = glam_mydiff1(x,y)
-    val
-}
-rcpp_glam_mydiff2 <- function(x,y) {
-    val = glam_mydiff2(x,y)
-    val
-}
-rcpp_glam_mydiff3 <- function(x,y) {
-    val = glam_mydiff3(x,y)
-    val
-}
-rcpp_glam_scorefunctheta1 <- function(x,y) {
-    val = glam_scorefunctheta1(x,y)
-    val
-}
-rcpp_glam_scorefuncmu1 <- function(x,y) {
-    val = glam_scorefuncmu1(x,y)
-    val
-}
-rcpp_glam_scorefunctheta2 <- function(x,y) {
-    val = glam_scorefunctheta2(x,y)
-    val
-}
-rcpp_glam_scorefuncmu2 <- function(x,y) {
-    val = glam_scorefuncmu2(x,y)
-    val
-}
-rcpp_glam_scorefunctheta3 <- function(x,y) {
-    val = glam_scorefunctheta3(x,y)
-    val
-}
-rcpp_glam_scorefuncmu3 <- function(x,y) {
-    val = glam_scorefuncmu3(x,y)
-    val
-}
+# rcpp_glam_myhfunc1 <- function(x,y) {
+#     val = glam_myhfunc1(x,y)
+#     val
+# }
+# rcpp_glam_myhfunc2 <- function(x,y) {
+#     val = glam_myhfunc2(x,y)
+#     val
+# }
+# rcpp_glam_myhfunc3 <- function(x,y) {
+#     val = glam_myhfunc3(x,y)
+#     val
+# }
+# rcpp_glam_myhfunc4 <- function(x,y) {
+#     val = glam_myhfunc4(x,y)
+#     val
+# }
+# rcpp_glam_myhfunc5 <- function(x,y) {
+#     val = glam_myhfunc5(x,y)
+#     val
+# }
+# rcpp_glam_myhfunc6 <- function(x,y) {
+#     val = glam_myhfunc6(x,y)
+#     val
+# }
+# rcpp_glam_myslopefunc <- function(x) {
+#     val = glam_myslopefunc(x)
+#     val
+# }
+# rcpp_glam_mydiff1 <- function(x,y) {
+#     val = glam_mydiff1(x,y)
+#     val
+# }
+# rcpp_glam_mydiff2 <- function(x,y) {
+#     val = glam_mydiff2(x,y)
+#     val
+# }
+# rcpp_glam_mydiff3 <- function(x,y) {
+#     val = glam_mydiff3(x,y)
+#     val
+# }
+# rcpp_glam_scorefunctheta1 <- function(x,y) {
+#     val = glam_scorefunctheta1(x,y)
+#     val
+# }
+# rcpp_glam_scorefuncmu1 <- function(x,y) {
+#     val = glam_scorefuncmu1(x,y)
+#     val
+# }
+# rcpp_glam_scorefunctheta2 <- function(x,y) {
+#     val = glam_scorefunctheta2(x,y)
+#     val
+# }
+# rcpp_glam_scorefuncmu2 <- function(x,y) {
+#     val = glam_scorefuncmu2(x,y)
+#     val
+# }
+# rcpp_glam_scorefunctheta3 <- function(x,y) {
+#     val = glam_scorefunctheta3(x,y)
+#     val
+# }
+# rcpp_glam_scorefuncmu3 <- function(x,y) {
+#     val = glam_scorefuncmu3(x,y)
+#     val
+# }
