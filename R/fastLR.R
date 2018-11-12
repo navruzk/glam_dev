cdf84dd563066602f780c441b507009b0c316ffa
navruzbek <40276486+navruzbek1992@users.noreply.github.com>
2018-11-12 16:05:49 +0900

    Update fastLR.R

--- R/fastLR.R
+++ R/fastLR.R
@@ -3,5 +3,5 @@ fastLR <- function(eps_f, eps_g)
     fastLR_(eps_f, eps_g)
 }
 library(Rcpp)
-sourceCpp(/src/glam_add_cpp.cpp)
+sourceCpp(src/glam_add_cpp.cpp)
 rcpp_glam_add() = glam::glam_add_cpp()
