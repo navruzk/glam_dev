9568278cb04f5a4f9f3e076c3a180a3acbb700b6
navruzbek <40276486+navruzbek1992@users.noreply.github.com>
2018-11-12 14:14:11 +0900

    Update glam_add_cpp.cpp

--- src/glam_add_cpp.cpp
+++ src/glam_add_cpp.cpp
@@ -2,7 +2,7 @@
 
 using namespace Rcpp;
 // [[Rcpp::export]]
-Numeric glam_add_cpp(int x, int y){
+NumericVector glam_add_cpp(int x, int y){
                 int total;
                 total = x + y;
                 return total;}
