769d6ff469538c1e8e0306e449c5d4263a8ecb56
navruzbek1992 <40276486+navruzbek1992@users.noreply.github.com>
2018-11-12 11:06:47 +0900

    Update glam_timestwo.cpp

--- src/glam_timestwo.cpp
+++ src/glam_timestwo.cpp
@@ -3,6 +3,7 @@
 using namespace Rcpp;
 // [[Rcpp::export]]
 NumericVector glam_add(int x, int y){
+                int total;
                 total = x + y;
                 return total;}
 // [[Rcpp::export]]
